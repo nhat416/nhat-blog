0830049c280d8b6cb3582ecd937a0f67feb8cf25
Nhat Tran <nhatt416@gmail.com>
2023-02-20 18:18:58 -0500

    chore(dep): add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
 // https://astro.build/config
 import react from "@astrojs/react";
-
 // https://astro.build/config
 import image from "@astrojs/image";
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [sitemap(), react(), image()]
+  integrations: [sitemap(), react(), image(), tailwind()]
 });
