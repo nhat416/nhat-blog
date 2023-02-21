6d1ce56f5d75dac834f4298693ea1f4bda1371e7
Nhat Tran <nhatt416@gmail.com>
2023-02-21 07:35:03 -0500

    fix(meta): add ThemeToggleButton

---
+++ src/components/ThemeToggleButton.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+
+const themes = ['light', 'dark']
+
+export default function ThemeToggle() {
+  const [isMounted, setIsMounted] = useState(false)
+  const [theme, setTheme] = useState(() => {
+    if (import.meta.env.SSR) {
+      return undefined
+    }
+    if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
+      return localStorage.getItem('theme')
+    }
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+       return 'dark'
+    }
+    return 'light'
+  })
+  const toggleTheme = () => {
+    const t = theme === 'light' ? 'dark' : 'light'
+    localStorage.setItem('theme', t)
+    setTheme(t)
+  }
+  useEffect(() => {
+    const root = document.documentElement
+    if (theme === 'light') {
+      root.classList.remove('dark')
+    } else {
+      root.classList.add('dark')
+    }
+  }, [theme])
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  return isMounted ? (
+    <div className='inline-flex items-center p-[1px]'>
+      {themes.map(t => {
+         const checked = t === theme
+         return (
+          <button key={t} className=>{t}</button>
+         )
+      })}
+    </div>
+  ) : (<div />)
+}
